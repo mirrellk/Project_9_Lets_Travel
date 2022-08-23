a665f7d6c52f434a024ed95ba6058b8ead88044d
mirrellk <mirrellk@verizon.net>
2022-08-23 07:12:54 -0400

    Update

--- app.js
+++ app.js
@@ -59,4 +59,6 @@ app.get('/login', (req, resp) => {
     }
 })
 
-app.listen(3000, () => console.log('Listening 3000...'));
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening ${port}...'));
+//app.listen(3000, () => console.log('Listening 3000...'));
